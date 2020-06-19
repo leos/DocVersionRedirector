2dba13fcf344c5b439e19c3f1a8e86cf7e2b6667
Leo Shklovskii <leo@beeswax.com>
2020-06-19 17:58:29 -0400

    updated versions

--- src/sites.ts
+++ src/sites.ts
@@ -173,6 +173,7 @@ class SiteConfig {
             options: {
                 version: [
                     'master',
+                    '3.3.0',
                     '3.2.0',
                     '3.1.0',
                     '3.0.0',
@@ -206,14 +207,14 @@ class SiteConfig {
             template: '/${lang}/${version}/${path}',
             options: {
                 lang: ['en', 'el', 'es', 'fr', 'id', 'ja', 'ko', 'pl', 'pt-br', 'zh-hans'],
-                version: ['dev', '3.0', '2.2', '2.1', '2.0', '1.11', '1.10', '1.8'],
+                version: ['dev', '3.1', '3.0', '2.2', '2.1', '2.0', '1.11', '1.10', '1.8'],
             },
         },
         'docs.python.org': {
             regex: '^/(?<version>[^/]*)/(?<path>.*)',
             template: '/${version}/${path}',
             options: {
-                version: ['3', '3.9', '3.8', '3.7', '3.6', '3.5', '2.7'],
+                version: ['3', '3.10', '3.9', '3.8', '3.7', '3.6', '3.5', '2.7'],
             },
             moves: [
                 {
