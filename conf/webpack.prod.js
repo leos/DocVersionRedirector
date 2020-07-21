bdc701ddcffddb8c2a2ce2bad890603f24f41fb0
Leo Shklovskii <214370+leos@users.noreply.github.com>
2020-07-20 23:50:32 -0400

    working updates

--- conf/webpack.prod.js
+++ conf/webpack.prod.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge')
+const {merge} = require('webpack-merge')
 const {project_base} = require('./project-base')
 const common = require('./webpack.common.js')
 const ZipPlugin = require('zip-webpack-plugin')
