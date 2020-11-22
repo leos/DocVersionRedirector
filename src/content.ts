2d04655acc43b5d30dd0d84648c86b5b895b6c77
Leo Shklovskii <leo@beeswax.com>
2020-11-21 19:58:47 -0500

    version updates

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-ts-ignore */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 // eslint-disable-next-line no-undef
 ;(typeof browser === 'undefined' ? chrome : browser).runtime.sendMessage({
