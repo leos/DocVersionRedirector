4cd95aefeb3b07b81c44208c038bd2df15781788
Leo Shklovskii <leo@beeswax.com>
2021-06-28 11:21:58 -0400

    upgrading lots of versions

--- src/content.ts
+++ src/content.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/ban-ts-comment,@typescript-eslint/no-extra-semi */
 // @ts-ignore
 // eslint-disable-next-line no-undef
 ;(typeof browser === 'undefined' ? chrome : browser).runtime.sendMessage({
--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import {browser} from 'webextension-polyfill-ts'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const w: any = browser.extension.getBackgroundPage()
 export const blog = w.console.log
