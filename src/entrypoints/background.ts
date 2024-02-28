1300aa057ecc452d44a715ad75897f3b33623a02
Leo Shklovskii <214370+leos@users.noreply.github.com>
2024-02-28 08:31:25 -0500

    fixing background loading

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,25 +1,24 @@
 import { SiteDefinitionConfigured } from "@/utils/site_types"
 
 export default defineBackground(() => {
-})
+    browser.runtime.onInstalled.addListener(async ({ reason }) => {
+        // console.log("Reason:", reason)
+        const configs = await storedConfigs.getValue()
 
-browser.runtime.onInstalled.addListener(async ({ reason }) => {
-    // console.log("Reason:", reason)
-    const configs = await storedConfigs.getValue()
+        const configuredDefs: SiteDefinitionConfigured[] = configs.flatMap(config => {
+            const d = siteDefinitions.find(d => d.id === config.id)
+            return d ? [{ ...d, config: { version: config.v } }] : []
+        })
 
-    const configuredDefs: SiteDefinitionConfigured[] = configs.flatMap(config => {
-        const d = siteDefinitions.find(d => d.id === config.id)
-        return d ? [{ ...d, config: { version: config.v } }] : []
+        const removeIds = configuredDefs.map(c => c.id)
+        const addRules = configuredDefs.map(c => createRuleForSite(c, c.config.version))
+
+        await browser.declarativeNetRequest.updateDynamicRules({
+            removeRuleIds: removeIds,
+            addRules: addRules,
+        })
+
+        // const rules = await browser.declarativeNetRequest.getDynamicRules()
+        // console.log("Rules: ", rules)
     })
-
-    const removeIds = configuredDefs.map(c => c.id)
-    const addRules = configuredDefs.map(c => createRuleForSite(c, c.config.version))
-
-    await browser.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: removeIds,
-        addRules: addRules,
-    })
-
-    const rules = await browser.declarativeNetRequest.getDynamicRules()
-    // console.log("Rules: ", rules)
 })
